b0fe1273e3ad98c8fd1e5e0c84b7911d0d94302b
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2016-05-11 19:13:44 -0300

    add messages

--- paw/webapp/src/main/java/ar/edu/itba/webapp/controller/BaseController.java
+++ paw/webapp/src/main/java/ar/edu/itba/webapp/controller/BaseController.java
@@ -1,8 +1,13 @@
 package ar.edu.itba.webapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,6 +24,11 @@ public abstract class BaseController {
 
 	@Autowired
 	private UserService us;
+	
+	@ModelAttribute
+	public Locale currentLanguage() {
+		return LocaleContextHolder.getLocale();
+	}
 	
 	@ModelAttribute
 	public User user() {
