828ccaab4e1d70f603e5747e909d1286bb68f3a7
sebamaio <sebamaio@gmail.com>
2016-05-20 16:41:43 -0300

    Tests are now hibernate-friendly

--- paw/services/src/test/java/ar/edu/itba/services/UserServiceImplTest.java
+++ paw/services/src/test/java/ar/edu/itba/services/UserServiceImplTest.java
@@ -10,12 +10,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.itba.models.User;
 
-@Sql("classpath:schema.sql")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class UserServiceImplTest {
@@ -34,6 +33,7 @@ public class UserServiceImplTest {
 	private UserServiceImpl us;
 
 	@Before
+	@Transactional
 	public void setup() {
 		if (!us.usernameExists(name)) {
 			user = us.create(name, password, mail);
@@ -41,87 +41,98 @@ public class UserServiceImplTest {
 		user = us.getByUsername(name);
 	}
 
-	@After
-	public void endingSetup() {
-		
-	}
-
 	@Test
+	@Transactional
    	public void createUserTest() {
 		assertNotNull(user);
    	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithNullName(){
 		us.create(null, password, mail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void creatWithNullPassword(){
 		us.create(name, null, mail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithNullMail(){
 		us.create(name, password, null);
 	}
 	
 	@Test(expected = IllegalStateException.class)
+	@Transactional
 	public void createWithUsedName(){
 		us.create(name, password, unusedMail);
 	}
 	
 	@Test(expected = IllegalStateException.class)
+	@Transactional
 	public void createWithUsedMail(){
 		us.create(unusedName, password, mail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithEmptyPassword(){
 		us.create(unusedName, empty, unusedMail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithEmptyName(){
 		us.create(empty, password, unusedMail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithEmptyMail(){
 		us.create(unusedName, password, empty);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithLongName(){
 		us.create(longString, password, unusedMail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithLongPassword(){
 		us.create(unusedName , longString, unusedMail);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void createWithLongMail(){
 		us.create(unusedName, password, longString + unusedMail);
 	}	
 	
 	@Test 
+	@Transactional
 	public void getByUsernameTest(){
 		assertTrue(us.getByUsername(user.username()).equals(user));
 	}
 	
 	@Test(expected = IllegalStateException.class)
+	@Transactional
 	public void getByinvalidUserName(){
 		us.getByUsername("Fake it!");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void getByusernameWithNull(){
 		us.getByUsername(null);
 	}
 	
 	@Test
+	@Transactional
 	public void getUsernamesTest(){
 		int i;
 		for(i = 100; i < 1100; i++){
@@ -131,12 +142,14 @@ public class UserServiceImplTest {
 	}
 	
 	@Test
+	@Transactional
 	public void usernameExistsTest(){
 		assertTrue(us.usernameExists(user.username()));
 		assertFalse(us.usernameExists("fake"));
 	}
 	
 	@Test
+	@Transactional
 	public void usernameExistsWithNull(){
 		assertTrue(us.emailExists(user.mail()));
 		assertFalse(us.emailExists("fake"));
@@ -144,11 +157,13 @@ public class UserServiceImplTest {
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void emailExistsTest(){
 		us.emailExists(null);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
+	@Transactional
 	public void emailExistsWithNull(){
 		us.usernameExists(null);
 	}
