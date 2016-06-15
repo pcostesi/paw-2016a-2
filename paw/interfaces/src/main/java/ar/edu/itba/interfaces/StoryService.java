a25417a1f7795ff57004b6091a71642a3627f9e2
Braulio Sespede <bsespede@itba.edu.ar>
2016-06-15 14:43:34 -0300

    Document service layer interfaces.

--- paw/interfaces/src/main/java/ar/edu/itba/interfaces/StoryService.java
+++ paw/interfaces/src/main/java/ar/edu/itba/interfaces/StoryService.java
@@ -9,18 +9,55 @@ import ar.edu.itba.models.Task;
 
 public interface StoryService {
 	
-	public Story create(final Iteration iteration, final String name);
+	/**
+	 * Create and persist a story instance
+	 * @param iteration Iteration where the story will belong to.
+	 * @param title Story title.
+	 * @return The persisted instance.
+	 */
+	public Story create(final Iteration iteration, final String title);
 	
+	/**
+	 * Get an story by its id.
+	 * @param storyId Story id.
+	 * @return The story instance.
+	 */
 	public Story getById(final int storyId);
 	
-	public Story setName(final Story story, final String name);
+	/**
+	 * Update the story title.
+	 * @param story Story to update title to.
+	 * @param title The new title. 
+	 * @return
+	 */
+	public Story setName(final Story story, final String title);
 	
+	/**
+	 * Delete story instance.
+	 * @param story Story instance to delete.
+	 */
 	public void deleteStory(final Story story);
 
+	/**
+	 * Get a map of stories with all their tasks.
+	 * @param iteration Iteration to get stories from.
+	 * @return A map of stories with all their tasks.
+	 */
 	public Map<Story, List<Task>> getStoriesWithTasksForIteration(final Iteration iteration);
 	
+	/**
+	 * Get parent iteration.
+	 * @param story To get the iteration from.
+	 * @return Parent iteration instance.
+	 */
 	public Iteration getParent(final Story story);
 
+	/**
+	 * Check if a story with the same title exist in the iteration.
+	 * @param iteration Iteration to check in.
+	 * @param title Title to check.
+	 * @return True if it has been used, false otherwise.
+	 */
 	public boolean storyExists(final Iteration iteration, final String title);
 	
 }
--- paw/interfaces/src/main/java/ar/edu/itba/interfaces/UserService.java
+++ paw/interfaces/src/main/java/ar/edu/itba/interfaces/UserService.java
@@ -7,22 +7,68 @@ import ar.edu.itba.models.User;
 
 public interface UserService {
 	
+	/**
+	 * Create and persist a user.
+	 * @param name Username of the new user.
+	 * @param password Password of the new user.
+	 * @param mail Mail of the new user.
+	 * @return The new user instance.
+	 */
 	public User create(final String name, final String password, final String mail);
 	
+	/**
+	 * Get user instance by its user name.
+	 * @param username User username.
+	 * @return Instance of the user.
+	 */
     public User getByUsername(final String username);
     
+    /**
+     * Get a list of all the users
+     * @return List of user instances.
+     */
     public List<String> getUsernames();
 
+    /**
+     * Checks if the username has been used.
+     * @param user Username to check.
+     * @return True if it exists, false otherwise.
+     */
 	public boolean usernameExists(final String user);
 
-	public boolean emailExists(final String user);
+	/**
+	 * Check if the email has been used.
+	 * @param mail Mail to check. 
+	 * @return True if mail has been used.
+	 */
+	public boolean emailExists(final String mail);
 
+	/**
+	 * Get a list of usernames exlucding the one from the user passed as parameter.
+	 * @param user User to exclude.
+	 * @return List of usernames.
+	 */
 	public List<String> getUsernamesExcept(final User user);
 
-	void editPassword(final User user, final String newPassword);
+	/**
+	 * Update user password.
+	 * @param user User to update.
+	 * @param newPassword The new password.
+	 */
+	public User editPassword(final User user, final String newPassword);
 
+	/**
+	 * Get list of project members usernames.
+	 * @param project Project to get members from.
+	 * @return List of usernames.
+	 */
 	public List<String> getUsernamesForProject(final Project project);
 
+	/**
+	 * Get a list of usernames that aren't members of the project.
+	 * @param project Project to exclude users from.
+	 * @return List of usernames.
+	 */
 	public List<String> getAvailableUsers(final Project project);
   
 }
