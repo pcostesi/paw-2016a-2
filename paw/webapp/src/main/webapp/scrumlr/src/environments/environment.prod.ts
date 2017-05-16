4f38e90aff1aa57854c2ea59bcf49dd9e28ebcfd
Pablo Alejandro Costesich <pablo.costesich@gmail.com>
2017-05-16 16:58:24 -0300

    add endpoint definitions

--- paw/webapp/src/main/webapp/scrumlr/src/environments/environment.prod.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  hmr: false
+  hmr: false,
+  api_endpoint: "http://pawserver.it.itba.edu.ar/grupo2"
+
 };
--- paw/webapp/src/main/webapp/scrumlr/src/environments/environment.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  hmr: true
+  hmr: true,
+  api_endpoint: "http://localhost:8080/paw"
 };
