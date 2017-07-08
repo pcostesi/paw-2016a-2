36cc625ad5f0a884382fa2013349d1fcc94cd746
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-07-08 21:05:53 +0000

    Merged in missing-page (pull request #123)
    Add not found component
    
    Approved-by: Sebastian Maio <sebamaio@gmail.com>

--- paw/webapp/src/main/webapp/scrumlr/src/app/app-routing.module.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginGuard } from './api';
 import { LandingComponent } from './landing/landing.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent, pathMatch: 'full' },
   { path: 'profile', loadChildren: './profile/profile.module#ProfileModule' },
   { path: 'project', canActivate: [LoginGuard], loadChildren: './project/project.module#ProjectModule' },
+  { path: '**', component: NotFoundComponent }
+
 ];
 
 @NgModule({
