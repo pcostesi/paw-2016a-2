359f6a234c8d363eec3080c2f4dab312d0d9b2e5
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-07-11 22:48:13 +0000

    Merged in dump_db (pull request #131)
    Add dump capabilities

--- paw/deploy.sh
+++ paw/deploy.sh
@@ -8,20 +8,51 @@ PASS="shiufi7T"
 PAWSERVER="10.16.1.110"
 APPWAR="app.war"
 
-mvn clean package
-cp webapp/target/webapp.war "${APPWAR}"
-chmod +rwx "${APPWAR}"
+function build_app() {
+    mvn clean package
+    cp webapp/target/webapp.war "${APPWAR}"
+    chmod +rwx "${APPWAR}"
+}
 
-# scp -o "ProxyCommand ssh ${PAMPERO} -W %h:%p" "${APPWAR}" "${GROUP}:${PASS}@${PAWSERVER}:/app/${APPWAR}"
-# sftp -o "ProxyCommand nc -X connect ${PAWSERVER}:22 %h %p" "${APPWAR}" "${PAMPERO}"
-echo "If prompted, password for group ${GROUP} is ${PASS}"
-sftp -o "ProxyJump ${PAMPERO}:22" "${GROUP}@${PAWSERVER}" <<_EOF
+function upload_app() {
+    echo "If prompted, password for group ${GROUP} is ${PASS}"
+    sftp -o "ProxyJump ${PAMPERO}" "${GROUP}@${PAWSERVER}" <<_EOF
 rm web/tmp-${APPWAR}
 rm web/${APPWAR}
 put ${APPWAR} web/tmp-${APPWAR}
 rename web/tmp-${APPWAR} web/${APPWAR}
 bye
 _EOF
+}
 
-# "${GROUP}@${PAWSERVER}:/app/${APPWAR}"
+function save_db() {
+    DATETIME=$(date +%s)
+    DUMP="${GROUP}-${DATETIME}.sql"
+    ssh "${PAMPERO}" <<_EOF
+    export PGPASSWORD="${PASS}"
+    pg_dump -U ${GROUP} -h ${PAWSERVER} > ${DUMP}
+_EOF
+    scp "${PAMPERO}":"${DUMP}" .
+}
 
+function all() {
+    build_app;
+    save_db;
+    upload_app;
+}
+
+function main() {
+    case "${1}" in
+        dump)
+        save_db;
+        ;;
+        build)
+        build_app;
+        ;;
+        *)
+        all;
+        ;;
+    esac
+}
+
+main $@;
