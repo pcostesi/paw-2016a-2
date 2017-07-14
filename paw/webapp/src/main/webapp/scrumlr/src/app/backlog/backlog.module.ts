f5c438dac3d6616497b30c2cf7908bd2d5efb6e2
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-07-14 17:55:01 +0000

    Merged in backlog-ui (pull request #143)
    Stub UI for the backlog module
    
    Approved-by: Sebastian Maio <sebamaio@gmail.com>

--- paw/webapp/src/main/webapp/scrumlr/src/app/backlog/backlog.module.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/app/backlog/backlog.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BacklogRoutingModule } from './backlog-routing.module';
+import { MainComponent } from './main/main.component';
+import { DetailComponent } from './detail/detail.component';
+import { CompactComponent } from './compact/compact.component';
+import { EditComponent } from './edit/edit.component';
+import { CreateComponent } from './create/create.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BacklogRoutingModule
   ],
-  declarations: []
+  declarations: [MainComponent, DetailComponent, CompactComponent, EditComponent, CreateComponent, DeleteComponent]
 })
 export class BacklogModule { }
---
+++ paw/webapp/src/main/webapp/scrumlr/src/app/backlog/compact/compact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'scrumlr-compact',
+  templateUrl: './compact.component.html',
+  styleUrls: ['./compact.component.scss']
+})
+export class CompactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
