725056891ce9f00af60fcf41b38d27756e95d695
Pablo Alejandro Costesich <rhfixer@gmail.com>
2017-12-21 02:57:10 +0000

    Merged in reassign-tab-order (pull request #188)
    Reassign tab order
    
    * Reassign tab order
    
    * correct semantic element
    
    * use project feed instead of user feed
    
    Approved-by: Pablo Alejandro Costesich <rhfixer@gmail.com>

--- paw/webapp/src/main/webapp/scrumlr/src/app/feed/feed-list/feed-list.component.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/app/feed/feed-list/feed-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FeedService } from 'app/feed/feed.service';
 import { FeedEvent } from 'app/feed/feed-event';
+import { Project } from 'app/project/project';
 
 @Component({
   selector: 'app-feed-list',
@@ -8,12 +9,25 @@ import { FeedEvent } from 'app/feed/feed-event';
   styleUrls: ['./feed-list.component.scss']
 })
 export class FeedListComponent implements OnInit {
+  private _project: Project;
+
   public feed: FeedEvent[] = [];
 
   constructor(private feedService: FeedService) { }
 
   ngOnInit() {
-    this.feedService.getFeedForUser().subscribe(feed => this.feed = feed);
+  }
+
+  @Input()
+  set project(project: Project) {
+    if (project) {
+      this._project = project;
+      this.feedService.getFeedForProject(project).subscribe(feed => this.feed = feed);
+    }
+  }
+
+  get project() {
+    return this._project;
   }
 
 }
--- paw/webapp/src/main/webapp/scrumlr/src/app/feed/feed.service.ts
+++ paw/webapp/src/main/webapp/scrumlr/src/app/feed/feed.service.ts
@@ -4,6 +4,7 @@ import { FeedEvent } from 'app/feed/feed-event';
 import { Observable } from 'rxjs/Observable';
 import { UserProfile } from 'app/api/account.service';
 import { BaseFeedEvent } from 'app/feed/base-feed-event';
+import { Project } from 'app/project/project';
 
 @Injectable()
 export class FeedService {
@@ -13,6 +14,17 @@ export class FeedService {
   getFeedForUser() {
 
     return this.api.get(`/feed`).map(response => {
+      if (response.ok) {
+        const parsed = response.json()
+        return <FeedEvent[]>parsed.events;
+      }
+      throw Error(response.statusText || response.status.toString());
+    });
+  }
+
+  getFeedForProject(project: Project) {
+
+    return this.api.get(`/feed/project/${project.code}`).map(response => {
       if (response.ok) {
         const parsed = response.json()
         return <FeedEvent[]>parsed.events;
